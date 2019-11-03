273fe9fdaf0451ce1abf8a909ec6d52f524d8b01
Vu Quang Nguyen <vuquangnguyen2019@yahoo.com>
2019-11-03 11:45:42 +0700

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,211 +1,262 @@
-'use strict';
+/**
+ * 
+ * Run 'grunt' to generate JS and CSS in folder 'dist' and site in folder '_site'
+ * *
+ * Run 'grunt watch' to automatically regenerate '_site' when you change files in 'src' or in 'website'
+ * 
+ */
+
 module.exports = function(grunt) {
-    // Load all grunt tasks
-    require('load-grunt-tasks')(grunt);
 
-    // Show elapsed time at the end
-    require('time-grunt')(grunt);
+  'use strict';
 
-    grunt.loadNpmTasks('grunt-umd');
-    grunt.loadNpmTasks('grunt-banner');
+  var jekyllConfig = "isLocal : false \r\n"+
+      "permalink: /:title/ \r\n"+
+      "exclude: ['.json', '.rvmrc', '.rbenv-version', 'README.md', 'Rakefile'," +
+                "'changelog.md', 'compiler.jar', 'private', '.htaccess'," + 
+                "'photoswipe.sublime-project', 'photoswipe.sublime-workspace'] \r\n"+
 
-    // Project configuration.
-    grunt.initConfig({
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed GPLv3 */\n',
+      "auto: true \r\n"+
+      "pswpversion: <%= pkg.version %> \r\n"+
+      "siteversion: 1.0.4 \r\n"+
+      "markdown: redcarpet \r\n"+
+      "kramdown: \r\n"+
+      "  input: GFM \r\n";
 
-        // Task configuration.
-        clean: {
-            files: ['dist']
-        },
+  grunt.initConfig({
 
-        /* jshint ignore:start */
-        concat: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            basic_and_extras: {
-                files: {
-                    'dist/css/<%= pkg.name %>.css': ['src/css/<%= pkg.name %>.css'],
-                    'dist/css/lg-fb-comment-box.css': ['src/css/lg-fb-comment-box.css'],
-                    'dist/css/lg-transitions.css': ['src/css/lg-transitions.css'],
-                    'dist/js/<%= pkg.name %>.js': ['dist/js/<%= pkg.name %>.js'],
-                    'dist/js/<%= pkg.name %>-all.js': ['dist/js/<%= pkg.name %>.js', 'modules/lg-autoplay.js', 'modules/lg-fullscreen.js', 'modules/lg-pager.js', 'modules/lg-thumbnail.js', 'modules/lg-video.js', 'modules/lg-zoom.js', 'modules/lg-hash.js', 'modules/lg-share.js']
-                }
-            }
-        },
-        /* jshint ignore:end */
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                files: [{
-                    src: 'dist/js/<%= pkg.name %>.js',
-                    dest: 'dist/js/<%= pkg.name %>.min.js'
-                }, {
-                    src: ['dist/js/<%= pkg.name %>.js', 'modules/lg-autoplay.js', 'modules/lg-fullscreen.js', 'modules/lg-pager.js', 'modules/lg-thumbnail.js', 'modules/lg-video.js', 'modules/lg-zoom.js', 'modules/lg-hash.js', 'modules/lg-share.js'],
-                    dest: 'dist/js/<%= pkg.name %>-all.min.js'
-                }]
-            }
-        },
-        umd: {
-            all: {
-                options: {
-                    src: 'src/js/<%= pkg.name %>.js',
-                    dest: 'dist/js/<%= pkg.name %>.js',
-                    deps: {
-                        args : ['$'],
-                        'default': ['$'],
-                        amd: {
-                            indent: 6,
-                            items: ['jquery'],
-                            prefix: '\'',
-                            separator: ',\n',
-                            suffix: '\''
-                        },
-                        cjs: {
-                            indent: 6,
-                            items: ['jquery'],
-                            prefix: 'require(\'',
-                            separator: ',\n',
-                            suffix: '\')'
-                        },
-                        global: {
-                            items: ['jQuery'],
-                        },
-                        pipeline: {
-                            indent: 0,
-                            items : ['jquery'],
-                            prefix: '//= require ',
-                            separator: '\n',
-                        }
-                    }
-                }
-            }
-        },
+    pkg: grunt.file.readJSON('photoswipe.json'),
 
-        usebanner: {
-            taskName: {
-                options: {
-                    position: 'top',
-                    banner: '<%= banner %>',
-                    linebreak: true
-                },
-                files: {
-                    src: ['dist/js/<%= pkg.name %>.js']
-                }
-            }
-        },
-        cssmin: {
-            target: {
-                files: [{
-                    'dist/css/<%= pkg.name %>.min.css': ['src/css/<%= pkg.name %>.css']
-                }, {
-                    'dist/css/lg-fb-comment-box.min.css': ['src/css/lg-fb-comment-box.css']
-                },{
-                    'dist/css/lg-transitions.min.css': ['src/css/lg-transitions.css']
-                }]
-            }
-        },
-        copy: {
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/img/',
-                    src: ['**'],
-                    dest: 'dist/img/'
-                }, {
-                    expand: true,
-                    cwd: 'src/fonts/',
-                    src: ['**'],
-                    dest: 'dist/fonts/'
-                }]
-            }
-        },
-        qunit: {
-            all: {
-                options: {
-                    urls: ['http://localhost:9000/test/<%= pkg.name %>.html']
-                }
-            }
-        },
-        jshint: {
-            options: {
-                reporter: require('jshint-stylish')
-            },
-            gruntfile: {
-                options: {
-                    jshintrc: '.jshintrc',
-                    reporterOutput: ''
-                },
-                src: 'Gruntfile.js'
-            },
-            src: {
-                options: {
-                    jshintrc: 'src/js/.jshintrc',
-                    reporterOutput: ''
-                },
-                src: ['src/**/*.js']
-            },
-            test: {
-                options: {
-                    jshintrc: 'test/.jshintrc',
-                    reporterOutput: ''
-                },
-                src: ['test/**/*.js']
-            }
-        },
-        sass: {
-            dist: {
-                options: { // Target options
-                    style: 'expanded'
-                },
-                files: {
-                    'src/css/lightgallery.css': 'src/sass/lightgallery.scss'
-                }
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            src: {
-                files: '<%= jshint.src.src %>',
-                tasks: ['jshint:src', 'qunit']
-            },
-            test: {
-                files: '<%= jshint.test.src %>',
-                tasks: ['jshint:test', 'qunit']
-            },
-            css: {
-                files: 'src/**/*.scss',
-                tasks: ['sass']
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    hostname: '0.0.0.0',
-                    port: 9000
-                }
-            }
+    banner: '/*! PhotoSwipe - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; */\n',
+
+    defaultUIBanner:  '/*! PhotoSwipe Default UI - <%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; */\n',
+
+    // Task configuration.
+    clean: {
+      files: ['dist']
+    },
+    
+    sass: {                            
+      dist: {                      
+        files: {      
+          'dist/photoswipe.css': 'src/css/main.scss',
+          'dist/default-skin/default-skin.css': 'src/css/default-skin/default-skin.scss'
         }
+      }
+    },
+
+    // https://github.com/nDmitry/grunt-autoprefixer
+    autoprefixer: { 
+      options: {
+        browsers: ['last 3 versions', 'android 3', 'ie 9', 'bb 10']
+      },
+      no_dest: {
+        src: ['dist/photoswipe.css', 'dist/default-skin/default-skin.css']
+      }
+    },
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'dist/photoswipe.js',
+        'dist/photoswipe-ui-default.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    pswpbuild: {
+      all: {
+        src: [
+          'framework-bridge',
+          'core',
+          'gestures',
+          'show-hide-transition',
+          'items-controller',
+          'tap',
+          'desktop-zoom',
+          'history'
+        ],
+        basePath: 'src/js/',
+        dest: 'dist/photoswipe.js',
+        uidest: 'dist/photoswipe-ui-default.js',
+        banner: '<%= banner %>',
+        defaultUIBanner: '<%= defaultUIBanner %>'
+      }
+    },
+
+    jekyll: {
+      dev: {
+        options: {
+          src: 'website',
+          dest: '_site',
+          url: 'local',
+          raw: jekyllConfig + "url: local"
+        }
+        
+      },
+      production: {
+        options: {
+          src: 'website',
+          dest: '_production',
+          url: 'production',
+          raw: jekyllConfig + "url: production"
+        }
+      }
+    },
+
+    copy: {
+      dev: {
+        files: [
+          {src: ['src/css/default-skin/default-skin.svg'], dest: 'dist/default-skin/default-skin.svg'},
+          {src: ['src/css/default-skin/default-skin.png'], dest: 'dist/default-skin/default-skin.png'},
+          {src: ['src/css/default-skin/preloader.gif'], dest: 'dist/default-skin/preloader.gif'},
+          {expand: true, src: ['dist/**'], dest: '_site/'}
+        ]
+      },
+      main: {
+        files: [
+          {expand: true, src: ['dist/**'], dest: 'website/'}
+        ]
+      }
+
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'dist/photoswipe.min.js': ['dist/photoswipe.js'],
+          'dist/photoswipe-ui-default.min.js': ['dist/photoswipe-ui-default.js']
+        },
+        preserveComments: /^!/i
+      },
+      options: {
+        preserveComments: /^!/i
+      }
+    },
+
+    watch: { // for development run 'grunt watch'
+      jekyll: {
+        files: ['website/**', 'website/documentation/**', '_includes/**'],
+        tasks: ['jekyll:dev', 'copy:dev']
+      },
+      files: ['src/**'],
+      tasks: [ 'sass', 'autoprefixer', 'pswpbuild', 'copy:dev', 'uglify']
+    },
+
+    cssmin: {
+      compress: {
+        files: {
+          "website/site-assets/all.min.css": ["website/site-assets/site.css", "website/dist/photoswipe.css"]
+        }
+      }
+    },
+
+    svgmin: {
+      dist: {
+        files: {
+          'src/css/default-skin/default-skin.svg': 'src/css/default-skin/default-skin.svg'
+        }
+      }
+    }
+
+
+  });
+
+
+  // grunt pswpbuild --pswp-exclude=ajax,image
+  grunt.task.registerMultiTask('pswpbuild', 'Makes PhotoSwipe core JS file.', function() {
+
+    var files = this.data.src,
+        includes = grunt.option('pswp-exclude'),
+        basePath = this.data.basePath,
+        newContents = this.data.banner;
+
+    newContents += "(function (root, factory) { \n"+
+      "\tif (typeof define === 'function' && define.amd) {\n" +
+        "\t\tdefine(factory);\n" +
+      "\t} else if (typeof exports === 'object') {\n" +
+        "\t\tmodule.exports = factory();\n" +
+      "\t} else {\n" +
+        "\t\troot.PhotoSwipe = factory();\n" +
+      "\t}\n" +
+    "})(this, function () {\n\n" +
+      "\t'use strict';\n"+
+      "\tvar PhotoSwipe = function(template, UiClass, items, options){\n";
+      
+
+    if(includes) {
+      includes = includes.split(/[\s,]+/); // 'a,b,c' => ['a','b','c']
+      var removeA = function (arr) {
+          var what, a = arguments, L = a.length, ax;
+          while (L > 1 && arr.length) {
+              what = a[--L];
+              while ((ax= arr.indexOf(what)) !== -1) {
+                  arr.splice(ax, 1);
+              }
+          }
+          return arr;
+      };
+
+      includes.forEach(function( name ) {
+        if(name) {
+           
+           grunt.log.writeln( 'removed "'+name +'"' );
+           files = removeA(files, name);
+         }
+      });
+    }
+    
+    grunt.log.writeln( 'Your build is made of:'+files );
+
+    files.forEach(function( name ) {
+      // Wrap each module with a pience of code to be able to exlude it, stolen for modernizr.com
+      newContents += "\n/*>>"+name+"*/\n"; 
+      newContents += grunt.file.read( basePath + name + '.js' ) + '\n';
+      newContents += "\n/*>>"+name+"*/\n"; 
     });
 
-    // Default task.
-    grunt.registerTask('default', ['clean', 'jshint', 'connect', 'qunit', 'umd:all', 'concat','uglify', 'sass', 'cssmin', 'copy', /*'usebanner', 'watch'*/]);
-    grunt.registerTask('server', function() {
-        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-        grunt.task.run(['serve']);
-    });
 
-    grunt.registerTask('serve', ['connect', 'watch']);
-    grunt.registerTask('test', ['jshint', 'connect', 'qunit']);
+    newContents+= "\tframework.extend(self, publicMethods); };\n";
+    newContents+= "\treturn PhotoSwipe;\n";
+    newContents+= "});";
+
+
+    grunt.file.write( this.data.dest, newContents );
+
+    var uiContents = grunt.file.read( basePath + 'ui/photoswipe-ui-default.js' );
+    uiContents = this.data.defaultUIBanner + uiContents;
+    grunt.file.write( this.data.uidest, uiContents );
+  });
+
+
+
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-svgmin');
+
+  // Default task.
+  grunt.registerTask('default', ['sass', 'autoprefixer', 'pswpbuild','uglify', 'copy', 'jekyll:dev']);
+
+  grunt.registerTask('production', ['sass', 'autoprefixer', 'pswpbuild', 'uglify', 'copy', 'cssmin', 'jekyll:production']);
+  grunt.registerTask('nosite', ['sass', 'autoprefixer', 'pswpbuild', 'uglify']);
+  grunt.registerTask('hint', ['jshint']);
+
 };
